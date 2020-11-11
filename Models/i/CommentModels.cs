4d95748a1388f1c54afbb7ddb600e832809196ab
olcay <olcayparlakx@gmail.com>
2020-11-11 23:17:11 +0300

    i

---
+++ Models/i/CommentModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models.i
+{
+    public class CommentModels
+    {
+        public List<DB.Comments> Comments { get; set; }
+        public DB.Products Product { get; set; }
+    }
+}
